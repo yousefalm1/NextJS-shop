9a54ec168d006f21019c54ac0830b82bc649e47c
Yousef Almesaeed <yousefalm@Outlook.com>
2024-10-15 12:41:22 +0300

    Finsihed Task 2 pluging in products

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,51 @@
 import Image from "next/image";
+import products from "./products";
+
 import cookieStore from "../assets/images/cookie-store.jpg";
 
 const Home = () => {
   return (
-    <div className=" flex justify-center flex-col items-center ">
-      <h1 className=" text-7xl mt-36">The Dough Bros</h1>
-      <p className="mt-4 font-bold"> Best cookies in town</p>
-      <Image
-        src={cookieStore}
-        height={500}
-        width={500}
-        alt="Cookie Store"
-        className=" flex justify-center rounded-full my-6"
-      />
-    </div>
+    <>
+      <div className="flex justify-center flex-col items-center">
+        <div className="  ">
+          <h1 className=" text-7xl mt-36">The Dough Bros</h1>
+          <p className="mt-4 font-bold"> Best cookies in kuwait</p>
+          <Image
+            src={cookieStore}
+            height={500}
+            width={500}
+            alt="Cookie Store"
+            className=" flex justify-center rounded-full my-6"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-row gap-8 ">
+        <div className=" bg-black w-60 h-60 rounded-xl flex flex-col items-center justify-center">
+          <Image
+            src={products[0].image}
+            width={100}
+            height={10}
+            className="rounded-full py-4"
+            alt="Choco Chip"
+          />
+          <h1 className="">{products[0].name}</h1>
+          <p>{products[0].price}</p>
+        </div>
+
+        <div className=" bg-black w-60 h-60 rounded-xl flex flex-col items-center justify-center">
+          <Image
+            src={products[1].image}
+            width={100}
+            height={10}
+            className="rounded-full py-4"
+            alt="Choco Chip"
+          />
+          <h1 className="">{products[1].name}</h1>
+          <p>{products[1].price}</p>
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ src/app/products.js
@@ -0,0 +1,9 @@
+import originalCookie from "../assets/images/chocochip.jpeg";
+import chocolateCookie from "../assets/images/cookieOne.jpeg";
+
+const products = [
+  { image: originalCookie, name: "Dough-Migos", price: 2 },
+  { image: chocolateCookie, name: "Chip Buddies", price: 1.5 },
+];
+
+export default products;
